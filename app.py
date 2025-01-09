b02226034656e4dbc14b53e36fd4343ece07a329
root <root@DESKTOP-OHN0TFT.>
2025-01-09 14:50:20 -0300

    ix: chenge host

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def item_detail(item_id):
         return jsonify(message=f"Item {item_id} deleted"), 204
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
